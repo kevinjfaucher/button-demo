95474f44ed329146802af6b227903bcb10059a83
Kevin <kfaucher@dtcc.edu>
2023-10-08 22:56:50 -0400

    Updated to class based syntax

--- App.js
+++ App.js
@@ -1,52 +1,65 @@
-// Import necessary components and hooks from React and React Native.
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
-// Define the main App component.
-export default function App() {
-  // Declare a state variable 'message' with its initial value as an empty string.
-  // 'setMessage' is a function we can use to update this state.
-  const [message, setMessage] = useState('');
+// Define the main App component as a class.
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        // Initialize the state with a property 'message' set to an empty string.
+        this.state = {
+            message: ''
+        };
+    }
 
-  return (
-      <View style={styles.container}>
-        {/* Render three buttons. Each button has an onPress handler that updates the message when pressed. */}
-        <Button
-            title="Press me #1"
-            onPress={() => setMessage('You pressed the first button!')}
-        />
-        <Button
-            title="Press me #2"
-            onPress={() => setMessage('You pressed the second button!')}
-            // Apply a style to create space between the buttons.
-            style={styles.buttonSpacing}
-        />
-        <Button
-            title="Press me #3"
-            onPress={() => setMessage('You pressed the third button!')}
-            // Apply a style to create space between the buttons.
-            style={styles.buttonSpacing}
-        />
-        {/* Display the message below the buttons. The message changes depending on which button was last pressed. */}
-        <Text style={styles.messageText}>{message}</Text>
-      </View>
-  );
+    // These methods will update the state with the appropriate message based on which button was pressed.
+    handleButton1Press = () => {
+        this.setState({ message: 'You pressed the first button!' });
+    }
+
+    handleButton2Press = () => {
+        this.setState({ message: 'You pressed the second button!' });
+    }
+
+    handleButton3Press = () => {
+        this.setState({ message: 'You pressed the third button!' });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    title="Press me #1"
+                    onPress={this.handleButton1Press}
+                />
+                <Button
+                    title="Press me #2"
+                    onPress={this.handleButton2Press}
+                    style={styles.buttonSpacing}
+                />
+                <Button
+                    title="Press me #3"
+                    onPress={this.handleButton3Press}
+                    style={styles.buttonSpacing}
+                />
+                <Text style={styles.messageText}>{this.state.message}</Text>
+            </View>
+        );
+    }
 }
 
-// Define styles for the components.
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,                  // Ensure the container takes up the full available space.
-    justifyContent: 'center', // Center its children vertically.
-    paddingHorizontal: 20,    // Add horizontal padding.
-    backgroundColor: '#fff',  // Set the background color to white.
-  },
-  buttonSpacing: {
-    marginVertical: 10,       // Add vertical margin (above and below) to create spacing between buttons.
-  },
-  messageText: {
-    marginTop: 20,            // Add space above the message text.
-    textAlign: 'center',     // Center the text horizontally.
-    fontSize: 16,             // Set font size.
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#fff',
+    },
+    buttonSpacing: {
+        marginVertical: 10,
+    },
+    messageText: {
+        marginTop: 20,
+        textAlign: 'center',
+        fontSize: 16,
+    },
 });
